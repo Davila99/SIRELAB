c3ef9dcf5832d459eeca679e28d31db357cfc067
Davila99 <davilaeliseo453@gmail.com>
2023-05-18 20:35:34 -0600

    Update urls.py

--- SIRELAB/urls.py
+++ SIRELAB/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('registro/', include('registro.urls')),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    
     
 ]
